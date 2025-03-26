7cf24dc77bdc7a8766e1f714ab445ed2957b10ad
Haoxuan Li <64646379+DerKleineLi@users.noreply.github.com>
2025-03-27 00:04:17 +0100

    seperate cases whether in tripple up

--- reframework/autorun/Kinsect_dont_come_back.lua
+++ reframework/autorun/Kinsect_dont_come_back.lua
@@ -13,11 +13,15 @@ local saved_config = json.load_file("Kinsect_dont_come_back_settings.json") or {
 
 local config = {
     auto_back_time = 200.0,
-    charged_attack = true,
+    charged_attack_is_tripple = true, -- true means don't come back
+    charged_attack_not_tripple = false,
     helicopter = false,
-    aim_attack_start = true,
-    aim_attack_end = true,
-    wide_sweep = true,
+    aim_attack_start_is_tripple = true,
+    aim_attack_start_not_tripple = false,
+    aim_attack_end_is_tripple = true,
+    aim_attack_end_not_tripple = false,
+    wide_sweep_is_tripple = true,
+    wide_sweep_not_tripple = false,
 }
 
 merge_tables(config, saved_config)
@@ -88,6 +92,13 @@ sdk.hook(sdk.find_type_definition("app.Wp10Insect"):get_method("requestChangeAct
 function(args)
     local Wp10Insect = sdk.to_managed_object(args[2])
     if not Wp10Insect then return end
+    local hunter = Wp10Insect:get_Hunter()
+    if not hunter then return end
+    if not (hunter:get_IsMaster() and hunter:get_IsUserControl()) then return end
+
+    -- hunter state
+    local Wp10Handling = Wp10Insect._Wp10
+    local is_tripple_up = Wp10Handling:get_IsTrippleUp()
 
     -- auto back time
     local action_param = Wp10Insect._ActionParam
@@ -102,7 +113,7 @@ function(args)
     local index = sdk.get_native_field(action_id, sdk.find_type_definition("ace.ACTION_ID"), "_Index")
     -- log.debug("category: " .. category .. ", index: " .. index)
     if category == 0 and index == 17 then
-        if in_hold_attack and config.charged_attack then
+        if in_hold_attack and ((config.charged_attack_is_tripple and is_tripple_up) or (config.charged_attack_not_tripple and not is_tripple_up)) then
             return sdk.PreHookResult.SKIP_ORIGINAL
         end
         if in_baton_up_slash and config.helicopter then
@@ -110,12 +121,12 @@ function(args)
         end
     end
     if category == 0 and index == 13 then
-        if in_aim_attack and config.aim_attack_start then
+        if in_aim_attack and ((config.aim_attack_start_is_tripple and is_tripple_up) or (config.aim_attack_start_not_tripple and not is_tripple_up)) then
             return sdk.PreHookResult.SKIP_ORIGINAL
         end
     end
     if category == 0 and index == 4 then
-        if in_aim_attack_update and config.aim_attack_end then
+        if in_aim_attack_update and ((config.aim_attack_end_is_tripple and is_tripple_up) or (config.aim_attack_end_not_tripple and not is_tripple_up)) then
             local ActionIDType = sdk.find_type_definition("ace.ACTION_ID")
             local instance = ValueType.new(ActionIDType)
             sdk.set_native_field(instance, ActionIDType, "_Category", 0)
@@ -123,7 +134,7 @@ function(args)
             Wp10Insect:call("requestChangeAction(ace.ACTION_ID, System.Boolean)", instance, false)
             return sdk.PreHookResult.SKIP_ORIGINAL
         end
-        if in_baton_s_slash and config.wide_sweep then
+        if in_baton_s_slash and ((config.wide_sweep_is_tripple and is_tripple_up) or (config.wide_sweep_not_tripple and not is_tripple_up)) then
             return sdk.PreHookResult.SKIP_ORIGINAL
         end
     end
@@ -146,11 +157,15 @@ re.on_draw_ui(function()
     if imgui.tree_node("Kinsect, Don't Come Back!") then
         changed, config.auto_back_time = imgui.drag_float("Auto Back Time", config.auto_back_time, 0.1, 0, 200, "%.2f")
         imgui.text("Default: 3.0; Set to 200.0 to disable auto back")
-        changed, config.charged_attack = imgui.checkbox("Charged Attack", config.charged_attack)
+        changed, config.charged_attack_is_tripple = imgui.checkbox("Charged Attack in Tripple Up", config.charged_attack_is_tripple)
+        changed, config.charged_attack_not_tripple = imgui.checkbox("Charged Attack not Tripple Up", config.charged_attack_not_tripple)
         changed, config.helicopter = imgui.checkbox("Helicopter", config.helicopter)
-        changed, config.aim_attack_start = imgui.checkbox("Aim Attack Start", config.aim_attack_start)
-        changed, config.aim_attack_end = imgui.checkbox("Aim Attack End", config.aim_attack_end)
-        changed, config.wide_sweep = imgui.checkbox("Wide Sweep (in focus mode)", config.wide_sweep)
+        changed, config.aim_attack_start_is_tripple = imgui.checkbox("Aim Attack Start in Tripple Up", config.aim_attack_start_is_tripple)
+        changed, config.aim_attack_start_not_tripple = imgui.checkbox("Aim Attack Start not Tripple Up", config.aim_attack_start_not_tripple)
+        changed, config.aim_attack_end_is_tripple = imgui.checkbox("Aim Attack End in Tripple Up", config.aim_attack_end_is_tripple)
+        changed, config.aim_attack_end_not_tripple = imgui.checkbox("Aim Attack End not Tripple Up", config.aim_attack_end_not_tripple)
+        changed, config.wide_sweep_is_tripple = imgui.checkbox("Wide Sweep in Tripple Up", config.wide_sweep_is_tripple)
+        changed, config.wide_sweep_not_tripple = imgui.checkbox("Wide Sweep not Tripple Up", config.wide_sweep_not_tripple)
 
         imgui.tree_pop()
     end
